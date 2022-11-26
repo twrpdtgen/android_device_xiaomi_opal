f3f1f807007a7909ea4e2648e9ec68a3f6a9c6a2
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-11-26 12:05:13 +0100

    opal: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: xiaomi Model: opal Script version: 3.0.0

---
+++ omni_opal.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from opal device
+$(call inherit-product, device/xiaomi/opal/device.mk)
+
+PRODUCT_DEVICE := opal
+PRODUCT_NAME := omni_opal
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := opal
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="opal-user 11 RP1A.200720.011 V13.0.6.0.RGLMIXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/opal_global/opal:11/RP1A.200720.011/V13.0.6.0.RGLMIXM:user/release-keys
